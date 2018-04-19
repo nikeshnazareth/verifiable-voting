3fd79d52bb01f94b25241effbdbc42a815af0a6a
Nikesh Nazareth <nikesh.nazareth.developer@gmail.com>
2018-04-19 13:18:29 +1000

    Added Material Typography

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'vv-root',
   styleUrls: ['./app.component.scss'],
   template: `
-    <div class="mat-app-background">
+    <div class="mat-app-background" class="mat-typography">
       <h1>Verifiable Voting</h1>
     </div>
   `
