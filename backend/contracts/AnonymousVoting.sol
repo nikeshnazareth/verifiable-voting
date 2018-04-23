7b6aed463b2c561e9ee78b532f9efbc66a7e3a1e
Nikesh Nazareth <nikesh.nazareth.developer@gmail.com>
2018-04-23 16:57:50 +1000

    Added natspec comments to the AnonymousVoting and VoteListing contracts

--- backend/contracts/AnonymousVoting.sol
+++ backend/contracts/AnonymousVoting.sol
@@ -1,9 +1,21 @@
 pragma solidity ^0.4.21;
 
 
+/**
+    @title Anonymous Voting
+    @author Nikesh Nazareth
+    @notice Provides the current state of a vote
+    @dev A simple state machine allowing the participants to record their contributions to the vote.
+    @dev Each contribution is an IPFS hash pointing to the actual content
+*/
 contract AnonymousVoting {
     bytes32 public parametersHash;
 
+    /**
+        @notice Deploys the AnonymousVoting contract and sets the vote parameters
+        @notice (anything all users need to know about the vote)
+        @param _paramsHash the IPFS hash of the vote parameters
+    */
     function AnonymousVoting(bytes32 _paramsHash) public {
         parametersHash = _paramsHash;
     }
--- backend/contracts/VoteListing.sol
+++ backend/contracts/VoteListing.sol
@@ -3,13 +3,25 @@ import "./AnonymousVoting.sol";
 pragma solidity ^0.4.21;
 
 
+/**
+    @title VoteListing
+    @author Nikesh Nazareth
+    @notice Deploys AnonymousVote contracts, and maintains a list of the deployed contract addresses
+*/
 contract VoteListing {
     address[] public votingContracts;
 
+    /**
+        @notice Deploys a new AnonymousVote contract with the specified parameters
+        @param _paramsHash the IPFS hash of the vote parameters
+    */
     function deploy(bytes32 _paramsHash) public {
         votingContracts.push(new AnonymousVoting(_paramsHash));
     }
 
+    /**
+        @return the number of AnonymousVote contracts that have been deployed by this contract
+    */
     function numberOfVotingContracts() public constant returns (uint) {
         return votingContracts.length;
     }
