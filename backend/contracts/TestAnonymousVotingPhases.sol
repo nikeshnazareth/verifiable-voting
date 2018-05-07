3890e9096e39ff3fb07dae1747a78cabe4beadd7
Nikesh Nazareth <nikesh.nazareth.developer@gmail.com>
2018-05-07 16:32:59 +1000

    Closes #5. Added AnonymousVoting contract tests to cover the phase transitions

---
+++ backend/contracts/TestAnonymousVotingPhases.sol
@@ -0,0 +1,34 @@
+/*
+    This contract extends the AnonymousVoting contract in order to test the timing aspects.
+    It introduces functions to:
+    1. simulate the passage of time (by decrementing all time-related variables)
+    2. Call the updatePhase modifier
+ */
+
+import "./AnonymousVoting.sol";
+
+pragma solidity ^0.4.21;
+
+
+contract TestAnonymousVotingPhases is AnonymousVoting {
+
+    /// @notice pass all constructor parameters through to the AnonymousVoting constructor
+    function TestAnonymousVotingPhases(uint _registrationExpiration, uint _votingExpiration, string _paramsHash)
+    AnonymousVoting(_registrationExpiration, _votingExpiration, _paramsHash) public {}
+
+    /**
+        @notice Simulate the passage of time by subtracting the specified value from all time-related constants
+        @param _nSeconds the number of seconds to advance time
+    */
+    function advanceTime(uint _nSeconds) public {
+        registrationExpiration -= _nSeconds;
+        votingExpiration -= _nSeconds;
+    }
+
+    /**
+        Uses the "updatePhase" modifier to set the phase if necessary
+    */
+    function updatePhaseIfNecessary() public updatePhase {
+        // no-op
+    }
+}
--- backend/test/anonymousVoting.spec.js
+++ backend/test/anonymousVoting.spec.js
@@ -1,4 +1,5 @@
 const AnonymousVoting = artifacts.require('AnonymousVoting');
+const AnonymousVotingPhases = artifacts.require('TestAnonymousVotingPhases');
 
 describe('contract: AnonymousVoting', () => {
     const SUPPRESS_EXPECTED_ERROR = 'SUPPRESS_EXPECTED_ERROR';
@@ -7,8 +8,8 @@ describe('contract: AnonymousVoting', () => {
     let registrationExpiration;
     let votingExpiration;
     // arbitrary constants
-    const paramsHash = 'DUMMY_PARAMS_HASH';
-    const phaseDuration = 1000;
+    const PARAMS_HASH = 'DUMMY_PARAMS_HASH';
+    const PHASE_DURATION = 1000;
 
     describe('method: constructor', () => {
 
@@ -16,9 +17,9 @@ describe('contract: AnonymousVoting', () => {
 
             beforeEach(async () => {
                 const now = web3.eth.getBlock(web3.eth.blockNumber).timestamp;
-                registrationExpiration = now + phaseDuration;
-                votingExpiration = registrationExpiration + phaseDuration;
-                instance = await AnonymousVoting.new(registrationExpiration, votingExpiration, paramsHash);
+                registrationExpiration = now + PHASE_DURATION;
+                votingExpiration = registrationExpiration + PHASE_DURATION;
+                instance = await AnonymousVoting.new(registrationExpiration, votingExpiration, PARAMS_HASH);
             });
 
             it('should set the registrationExpiration time to the specified value', async () => {
@@ -31,26 +32,26 @@ describe('contract: AnonymousVoting', () => {
                 assert.equal(time.toNumber(), votingExpiration);
             });
 
-            it('should set currentPhase to 0 (Phase.Registration)', async () => {
+            it('should set currentPhase to Phase.Registration (0)', async () => {
                const phase = await instance.currentPhase.call();
                assert.equal(phase.toNumber(), 0);
             });
 
             it('should set parametersHash to the specified value', async () => {
                 const hash = await instance.parametersHash.call();
-                assert.equal(hash, paramsHash);
+                assert.equal(hash, PARAMS_HASH);
             });
         });
 
         describe('case: registration already expired', () => {
             beforeEach(() => {
                 const now = web3.eth.getBlock(web3.eth.blockNumber).timestamp;
-                registrationExpiration = now - phaseDuration;
-                votingExpiration = now + 2 * phaseDuration;
+                registrationExpiration = now - PHASE_DURATION;
+                votingExpiration = now + 2 * PHASE_DURATION;
             });
 
             it('should throw an error during deployment', done => {
-                AnonymousVoting.new(registrationExpiration, votingExpiration, paramsHash)
+                AnonymousVoting.new(registrationExpiration, votingExpiration, PARAMS_HASH)
                     .catch(() => SUPPRESS_EXPECTED_ERROR)
                     .then(val => val === SUPPRESS_EXPECTED_ERROR ? null :
                         Error('Successfully deployed AnonymousVoting contract with expired Registration phase')
@@ -62,17 +63,105 @@ describe('contract: AnonymousVoting', () => {
         describe('case: Voting phase expires before Registration phase', () => {
             beforeEach(() => {
                 const now = web3.eth.getBlock(web3.eth.blockNumber).timestamp;
-                registrationExpiration = now + 2 * phaseDuration;
-                votingExpiration = registrationExpiration - phaseDuration;
+                registrationExpiration = now + 2 * PHASE_DURATION;
+                votingExpiration = registrationExpiration - PHASE_DURATION;
             });
 
             it('should throw an error during deployment', done => {
-                AnonymousVoting.new(registrationExpiration, votingExpiration, paramsHash)
+                AnonymousVoting.new(registrationExpiration, votingExpiration, PARAMS_HASH)
                     .catch(() => SUPPRESS_EXPECTED_ERROR)
                     .then(val => val === SUPPRESS_EXPECTED_ERROR ? null :
                         Error('Successfully deployed AnonymousVoting contract with Voting ending before Registration')
                     )
                     .then(done);
+            });
+        });
+    });
+
+    describe('parameter: currentPhase', () => {
+
+        const PHASES = {
+            'REGISTRATION': 0,
+            'VOTING': 1,
+            'COMPLETE': 2
+        };
+
+        beforeEach(async () => {
+            const now = web3.eth.getBlock(web3.eth.blockNumber).timestamp;
+            registrationExpiration = now + PHASE_DURATION;
+            votingExpiration = registrationExpiration + PHASE_DURATION;
+            instance = await AnonymousVotingPhases.new(registrationExpiration, votingExpiration, PARAMS_HASH);
+        });
+
+        it('should start at Phase.Registration', async () => {
+            const phase = await instance.currentPhase.call();
+            assert.equal(phase.toNumber(), PHASES.REGISTRATION);
+        });
+
+        it('should stay at Phase.Registration if time does not pass', async () => {
+            await instance.updatePhaseIfNecessary();
+            const phase = await instance.currentPhase.call();
+            assert.equal(phase.toNumber(), PHASES.REGISTRATION);
+        });
+
+        describe('case: half the Registration phase duration passes', () => {
+            beforeEach(async () => {
+                await instance.advanceTime(PHASE_DURATION / 2);
+            });
+
+            it('should stay at Phase.Registration', async () => {
+                await instance.updatePhaseIfNecessary();
+                const phase = await instance.currentPhase.call();
+                assert.equal(phase.toNumber(), PHASES.REGISTRATION);
+            });
+        });
+
+        describe('case: slightly more than the Registration phase duration passes', () => {
+
+            beforeEach(async () => {
+                await instance.advanceTime(PHASE_DURATION * 1.1);
+            });
+
+            it('should advance to Phase.Voting', async () => {
+                await instance.updatePhaseIfNecessary();
+                const phase = await instance.currentPhase.call();
+                assert.equal(phase.toNumber(), PHASES.VOTING);
+            });
+
+            describe('case: consequently, half the Voting phase duration passes', () => {
+                beforeEach(async () => {
+                    await instance.advanceTime(PHASE_DURATION / 2);
+                });
+
+                it('should stay at Phase.Voting', async () => {
+                    await instance.updatePhaseIfNecessary();
+                    const phase = await instance.currentPhase.call();
+                    assert.equal(phase.toNumber(), PHASES.VOTING);
+                });
+            });
+
+            describe('case: consequently, the Voting phase duration passes', () => {
+                beforeEach(async () => {
+                    await instance.advanceTime(PHASE_DURATION);
+                });
+
+                it('should advance to Phase.Complete', async () => {
+                    await instance.updatePhaseIfNecessary();
+                    const phase = await instance.currentPhase.call();
+                    assert.equal(phase.toNumber(), PHASES.COMPLETE);
+                });
+            });
+        });
+
+        describe('case: more than the combined Registration and Voting phase durations passes', () => {
+            beforeEach(async () => {
+                await instance.advanceTime(2.1 * PHASE_DURATION);
+            });
+
+            it('should advance to Phase.Complete (bypassing Phase.Voting)', async () => {
+                await instance.updatePhaseIfNecessary();
+                const phase = await instance.currentPhase.call();
+                assert.equal(phase.toNumber(), PHASES.COMPLETE);
             });
         });
     });
