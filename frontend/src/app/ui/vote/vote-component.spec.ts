decbaecee8bcfd6167a2d16cf7f97d5644e8a94c
Nikesh Nazareth <nikesh.nazareth.developer@gmail.com>
2018-05-19 13:09:13 +1000

    Added overview info to the VoteComponent

--- frontend/src/app/ui/vote/vote-component.spec.ts
+++ frontend/src/app/ui/vote/vote-component.spec.ts
@@ -1,21 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import { VoteComponent } from './vote-component';
 import { MaterialModule } from '../../material/material.module';
+import { VoteRetrievalService } from '../../core/vote-retrieval/vote-retrieval.service';
+import { VotePhases } from '../../core/ethereum/anonymous-voting-contract/contract.api';
+import { Mock } from '../../mock/module';
+import { IVotingContractDetails } from '../../core/vote-retrieval/vote-retreival.service.constants';
 
 describe('Component: VoteComponent', () => {
   let fixture: ComponentFixture<VoteComponent>;
   let page: Page;
 
   class Page {
+    public static ARBITRARY_INDICES: number[] = [1, 2, 1, 0, 3];
+
     public container: DebugElement;
     public title: DebugElement;
+    public topic: DebugElement;
+    public phase: DebugElement;
+    public voteRetrievalSvc: VoteRetrievalService;
 
     constructor() {
       this.container = fixture.debugElement.query(By.css('.container'));
       this.title = this.container.query(By.css('h2'));
+      this.topic = this.container.query(By.css('.topic'));
+      this.phase = this.container.query(By.css('.phase'));
+      this.voteRetrievalSvc = fixture.debugElement.injector.get(VoteRetrievalService);
     }
   }
 
@@ -27,7 +41,9 @@ describe('Component: VoteComponent', () => {
       imports: [
         MaterialModule
       ],
-      providers: []
+      providers: [
+        {provide: VoteRetrievalService, useClass: Mock.VoteRetrievalService}
+      ]
     })
       .compileComponents()
       .then(() => {
@@ -37,22 +53,97 @@ describe('Component: VoteComponent', () => {
   }));
 
   describe('Structure', () => {
+
+    beforeEach(() => fixture.detectChanges());
+
     it('should have a container element', () => {
-      fixture.detectChanges();
       expect(page.container).toBeTruthy();
     });
 
     it('should have a title element', () => {
-      fixture.detectChanges();
       expect(page.title).toBeTruthy();
+    });
+
+    it('should have a topic element', () => {
+      expect(page.topic).toBeTruthy();
+    });
+
+    it('should have a phase element', () => {
+      expect(page.phase).toBeTruthy();
     });
   });
 
   describe('Title', () => {
     it('should state "Vote #[index]"', () => {
-      fixture.componentInstance.index = 2;
+      fixture.componentInstance.index = Page.ARBITRARY_INDICES[0];
       fixture.detectChanges();
-      expect(page.title.nativeElement.innerText).toEqual('Vote #2');
+      expect(page.title.nativeElement.innerText).toEqual('Vote #' + Page.ARBITRARY_INDICES[0]);
+    });
+
+    it('should update whenever index is changed', () => {
+      Page.ARBITRARY_INDICES.map(idx => {
+        fixture.componentInstance.index = Page.ARBITRARY_INDICES[idx];
+        fixture.detectChanges();
+        expect(page.title.nativeElement.innerText).toEqual('Vote #' + Page.ARBITRARY_INDICES[idx]);
+      });
+    });
+  });
+
+  describe('Topic', () => {
+    it('should state the specified vote topic', () => {
+      fixture.componentInstance.index = Page.ARBITRARY_INDICES[0];
+      fixture.detectChanges();
+      const topic: string = Mock.AnonymousVotingContractCollections[Page.ARBITRARY_INDICES[0]].parameters.topic;
+      expect(page.topic.nativeElement.innerText).toEqual(topic);
+    });
+
+    it('should update whenever index is changed', () => {
+      Page.ARBITRARY_INDICES.map(idx => {
+        fixture.componentInstance.index = Page.ARBITRARY_INDICES[idx];
+        fixture.detectChanges();
+        const topic: string = Mock.AnonymousVotingContractCollections[Page.ARBITRARY_INDICES[idx]].parameters.topic;
+        expect(page.topic.nativeElement.innerText).toEqual(topic);
+      });
+    });
+  });
+
+  fdescribe('Phase', () => {
+
+    it('should state the specified phase', () => {
+      fixture.componentInstance.index = Page.ARBITRARY_INDICES[0];
+      fixture.detectChanges();
+      const phase: number = Mock.AnonymousVotingContractCollections[Page.ARBITRARY_INDICES[0]].currentPhase;
+      expect(page.phase.nativeElement.innerText).toEqual(VotePhases[phase]);
+    });
+
+    it('should update whenever index is changed', () => {
+      Page.ARBITRARY_INDICES.map(idx => {
+        fixture.componentInstance.index = Page.ARBITRARY_INDICES[idx];
+        fixture.detectChanges();
+        const phase: number = Mock.AnonymousVotingContractCollections[Page.ARBITRARY_INDICES[idx]].currentPhase;
+        expect(page.phase.nativeElement.innerText).toEqual(VotePhases[phase]);
+      });
+    });
+
+    it('should update whenever the phase changes', () => {
+      // replace the phase parameter with a subject we control
+      const phase$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+      const details$: Observable<IVotingContractDetails> =
+        page.voteRetrievalSvc.detailsAtIndex$(Page.ARBITRARY_INDICES[0])
+          .combineLatest(phase$, (details, phase) => {
+            details.phase = VotePhases[phase];
+            return details;
+          });
+      spyOn(page.voteRetrievalSvc, 'detailsAtIndex$').and.returnValue(details$);
+
+      fixture.detectChanges();
+      let phaseIdx: number = Mock.AnonymousVotingContractCollections[Page.ARBITRARY_INDICES[0]].currentPhase;
+      expect(page.phase.nativeElement.innerText).toEqual(VotePhases[phaseIdx]);
+
+      phaseIdx++;
+      phase$.next(phaseIdx);
+      fixture.detectChanges();
+      expect(page.phase.nativeElement.innerText).toEqual(VotePhases[phaseIdx]);
     });
   });
 
@@ -78,7 +169,7 @@ describe('Component: VoteComponent', () => {
         expect(page.container.nativeElement.hidden).toEqual(false);
       });
 
-      it('sohuld stay visible with subsequent changes to input', () => {
+      it('should stay visible with subsequent changes to input', () => {
         fixture.detectChanges();
         fixture.componentInstance.index = 2;
         fixture.detectChanges();
--- frontend/src/app/ui/vote/vote-component.ts
+++ frontend/src/app/ui/vote/vote-component.ts
@@ -1,17 +1,52 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/pluck';
+
+import { VoteRetrievalService } from '../../core/vote-retrieval/vote-retrieval.service';
+import { IVotingContractDetails } from '../../core/vote-retrieval/vote-retreival.service.constants';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Component({
   selector: 'vv-vote',
   template: `
-    <div class="container" [hidden]="!_voteSelected()">
-      <h2>Vote #{{index}}</h2>
+    <div class="container" [hidden]="!_voteIsSelected">
+      <h2>Vote #{{_index$ | async}}</h2>
+      <div class="topic">{{_topic$ | async}}</div>
+      <div class="phase">{{_phase$ | async}}</div>
     </div>
   `,
 })
-export class VoteComponent {
-  @Input() index: number;
+export class VoteComponent implements OnInit {
+  private _voteIsSelected: boolean;
+  private _index$: BehaviorSubject<number>;
+  private _voteDetails$: Observable<IVotingContractDetails>;
+  private _topic$: Observable<string>;
+  private _phase$: Observable<string>;
 
-  private _voteSelected(): boolean {
-    return typeof(this.index) !== 'undefined';
+
+  constructor(private voteRetrievalSvc: VoteRetrievalService) {
+    this._index$ = new BehaviorSubject<number>(null);
+    this._voteIsSelected = false;
+  }
+
+  /**
+   * Initialise the observables used by the view
+   */
+  ngOnInit() {
+    this._voteDetails$ = this._index$
+      .switchMap(idx => this.voteRetrievalSvc.detailsAtIndex$(idx));
+
+    this._topic$ = this._voteDetails$.pluck('parameters').pluck('topic');
+    this._phase$ = this._voteDetails$.pluck('phase');
+  }
+
+  /**
+   * Convert the input "index" into an observable and set _voteIsSelected to enable the view
+   * @param val
+   */
+  @Input()
+  set index(val) {
+    this._index$.next(val);
+    this._voteIsSelected = typeof val !== 'undefined';
   }
 }
