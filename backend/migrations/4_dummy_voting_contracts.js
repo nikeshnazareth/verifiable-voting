bb664e382d92bd8877d0fe361b8dd62507c65b93
Nikesh Nazareth <nikesh.nazareth.developer@gmail.com>
2018-06-01 11:38:26 +1000

    Restricted dummy contract migrations to only occur on the develop network

--- backend/migrations/4_dummy_voting_contracts.js
+++ backend/migrations/4_dummy_voting_contracts.js
@@ -13,13 +13,15 @@ const paramHashes = [
 ];
 const registrationAuth = '0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef';
 
-module.exports = async function(deployer) {
-    const eligibilityContract = (await NoRestriction.deployed()).address;
-    const listing = await VoteListing.deployed();
-    // the first contract is in the registration phase for a week
-    await listing.deploy(now + 7 * day, now + 14 * day, paramHashes[0], eligibilityContract, registrationAuth);
-    // the second contract will soon be in the voting phase for week
-    await listing.deploy(now + sec, now + 7 * day, paramHashes[1], eligibilityContract, registrationAuth);
-    // the second contract will soon be in the complete phase
-    await listing.deploy(now + sec, now + 2 * sec, paramHashes[2], eligibilityContract, registrationAuth);
+module.exports = async function(deployer, network) {
+    if(network === 'develop') {
+        const eligibilityContract = (await NoRestriction.deployed()).address;
+        const listing = await VoteListing.deployed();
+        // the first contract is in the registration phase for a week
+        await listing.deploy(now + 7 * day, now + 14 * day, paramHashes[0], eligibilityContract, registrationAuth);
+        // the second contract will soon be in the voting phase for week
+        await listing.deploy(now + sec, now + 7 * day, paramHashes[1], eligibilityContract, registrationAuth);
+        // the second contract will soon be in the complete phase
+        await listing.deploy(now + sec, now + 2 * sec, paramHashes[2], eligibilityContract, registrationAuth);
+    }
 };
