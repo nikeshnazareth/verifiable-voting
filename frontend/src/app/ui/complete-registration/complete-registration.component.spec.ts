f1eda29289d98f1ff8eb78c5111538298bf682d0
nikeshnazareth <nikesh.nazareth.developer@gmail.com>
2018-09-25 12:43:45 +1000

    Added tests for the modulus and private exponent fields in the CompleteRegistrationComponent

--- frontend/src/app/ui/complete-registration/complete-registration.component.spec.ts
+++ frontend/src/app/ui/complete-registration/complete-registration.component.spec.ts
@@ -14,6 +14,7 @@ import { MaterialModule } from '../../material/material.module';
 import { DOMInteractionUtility } from '../../mock/dom-interaction-utility';
 import { Mock } from '../../mock/module';
 import { EthereumAddressValidatorTester } from '../../validators/ethereum-address.validator.tests';
+import { LowercaseHexValidatorTests } from '../../validators/lowercase-hex.validator.tests';
 import { CompleteRegistrationComponent } from './complete-registration.component';
 
 fdescribe('Component: CompleteRegistrationComponent', () => {
@@ -130,6 +131,90 @@ fdescribe('Component: CompleteRegistrationComponent', () => {
           spyOnProperty(page.web3Svc, 'defaultAccount').and.returnValue(undefined);
           DOMInteractionUtility.clickOn(button);
           expect(page.errSvc.add).toHaveBeenCalledWith(Web3Errors.account, null);
+        });
+      });
+    });
+
+    describe('Modulus', () => {
+      let inputBox: DebugElement;
+
+      beforeEach(() => {
+        fixture.detectChanges();
+        inputBox = fixture.debugElement.query(By.css('input[formControlName="modulus"'));
+      });
+
+      describe('input box', () => {
+        it('should exist', () => {
+          expect(inputBox).not.toBeNull();
+        });
+
+        it('should start empty', () => {
+          expect(inputBox.nativeElement.value).toBeFalsy();
+        });
+
+        it('should have a placeholder "Modulus"', () => {
+          expect(inputBox.nativeElement.placeholder).toEqual('Modulus');
+        });
+
+        it('should be a form control', () => {
+          expect(inputBox.attributes.formControlName).not.toBeNull();
+        });
+
+        describe('form control validity', () => {
+          let ctrl: AbstractControl;
+
+          const setValue = (value) => {
+            DOMInteractionUtility.setValueOn(inputBox, value);
+            fixture.detectChanges();
+          };
+
+          beforeEach(() => {
+            ctrl = fixture.componentInstance.form.get(inputBox.attributes.formControlName);
+          });
+
+          LowercaseHexValidatorTests.test(() => ctrl, setValue);
+        });
+      });
+    });
+
+    describe('Private Exponent', () => {
+      let inputBox: DebugElement;
+
+      beforeEach(() => {
+        fixture.detectChanges();
+        inputBox = fixture.debugElement.query(By.css('input[formControlName="privateExponent"'));
+      });
+
+      describe('input box', () => {
+        it('should exist', () => {
+          expect(inputBox).not.toBeNull();
+        });
+
+        it('should start empty', () => {
+          expect(inputBox.nativeElement.value).toBeFalsy();
+        });
+
+        it('should have a placeholder "Private Exponent"', () => {
+          expect(inputBox.nativeElement.placeholder).toEqual('Private Exponent');
+        });
+
+        it('should be a form control', () => {
+          expect(inputBox.attributes.formControlName).not.toBeNull();
+        });
+
+        describe('form control validity', () => {
+          let ctrl: AbstractControl;
+
+          const setValue = (value) => {
+            DOMInteractionUtility.setValueOn(inputBox, value);
+            fixture.detectChanges();
+          };
+
+          beforeEach(() => {
+            ctrl = fixture.componentInstance.form.get(inputBox.attributes.formControlName);
+          });
+
+          LowercaseHexValidatorTests.test(() => ctrl, setValue);
         });
       });
     });
